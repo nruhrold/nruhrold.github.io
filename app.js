f5247e8fb299641e23dca51bfc785013a0e230a8
nruhrold <nruhrold@gmail.com>
2017-06-13 22:13:31 -0400

    Initial file population
    Added: index.html, style.css, app.js, temp_img.jpg

---
+++ app.js
@@ -0,0 +1,9 @@
+function test() {
+	document.getElementById("sidebar").style.display = 'block';
+  document.getElementById("overlay").style.display = 'block';
+}
+
+function test2() {
+	document.getElementById("sidebar").style.display = 'none';
+  document.getElementById("overlay").style.display = 'none';
+}
